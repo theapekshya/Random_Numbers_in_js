5276d030efcebd55ad4c2e9f3245b3f1af2d5d82
theapekshya <139621450+theapekshya@users.noreply.github.com>
2023-11-07 21:51:53 +0545

    Random_Numbers_in_js

---
+++ random.js
@@ -0,0 +1,13 @@
+// Random numbers in js
+
+let x = Math.random();   //random value from 0 to 1 
+document.write(x,"<br>");
+
+let y = Math.random()*10;  // random value from 0 to 9
+document.write(y,"<br>");
+
+let z = Math.floor(Math.random()*10); // random value from 0 to 9 without decimal
+document.write(z,"<br>")
+
+let w = Math.floor(Math.random()*11);  // 0 to 10 
+document.write(w);
